f788a61c4593290cbd1b5bf3db0137c5480bea21
fRenzy <fRenzyA05@gmail.com>
2022-09-14 13:46:49 -0300

    Initial commit

---
+++ HelloWorld6-8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld6_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+            Console.ReadKey(true);
+        }
+    }
+}
